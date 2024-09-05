48d729f8866dde94a2b5d837aa28bbf7f7a9aadc
ChandupaJay1 <chandupajayalath33@gmail.com>
2024-09-05 22:53:40 +0530

    added Product listing

--- src/java/model/Validation.java
+++ src/java/model/Validation.java
@@ -4,13 +4,32 @@ package model;
 
 public class Validation {
     
-    public static boolean isEmailValid(String email){
+    public static boolean isEmailValid(String email) {
+        if (email == null) {
+            return false;
+        }
         return email.matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
     }
-    
-    public static boolean isPasswordValid(String password){
-    
-    return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$");
+
+    public static boolean isPasswordValid(String password) {
+        if (password == null) {
+            return false;
+        }
+        return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$");
+    }
+
+    public static boolean isDouble(String text) {
+        if (text == null) {
+            return false;
+        }
+        return text.matches("^\\d+(\\.\\d{2})?$");
+    }
+
+    public static boolean isInteger(String text) {
+        if (text == null) {
+            return false;
+        }
+        return text.matches("^\\d+$");
     }
     
 }
