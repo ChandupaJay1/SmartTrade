fb650aa947dc8d56a39a3faac396b6d38a3703ff
ChandupaJay1 <chandupajayalath33@gmail.com>
2024-09-11 15:45:36 +0530

    all done

--- src/java/controller/VerifyPayment.java
+++ src/java/controller/VerifyPayment.java
@@ -18,11 +18,12 @@ public class VerifyPayment extends HttpServlet {
         String merchant_id = request.getParameter("merchant_id");
         String order_id = request.getParameter("order_id");
         String payhere_amount = request.getParameter("payhere_amount");
+//String payhere_amount = request.getParameter("3000");
         String payhere_currency = request.getParameter("payhere_currency");
         String status_code = request.getParameter("status_code");
         String md5sig = request.getParameter("md5sig");
 
-        String merchant_secret = "Mzc0ODExNDMzNzI0ODgyMzU2MzMzODI0ODM3NzU4MjMwMTUyMDQ=";
+        String merchant_secret = "MzE2MDA0NDQ3ODMyNjQyMzMzMzkxNzY3MzI5Njk5MzU2NzgxMjAwMA==";
         String merchant_secret_md5hash = Payhere.generateMD5(merchant_secret);
 
         String generated_md5hash = Payhere.generateMD5(
